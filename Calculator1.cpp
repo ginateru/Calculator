cd04879531260e62244b365c2292798ba93d9d13
Natalya <ginateru@gmail.com>
2017-12-13 22:29:52 +0300

    skelet i prochee v develope

---
+++ Calculator1.cpp
@@ -0,0 +1,46 @@
+//פאיכ program.cpp 
+
+#include "stdafx.h"
+#include <iostream>
+#include "funcs.h"
+
+using namespace std;
+
+int main()
+{
+	int a, b, n;
+
+	cout << "Vvedite a: ";
+	cin >> a;
+	cout << "Vvedite b: ";
+	cin >> b;
+
+	cout << "Vvedite ¹ operacii. 1 - slojenie, 2 - vichitanie, 3 - umnojenie, 4 - delenie" << endl;
+	cin >> n;
+
+
+	switch(n)
+		{  
+         case 1:  
+            cout << "Summa ravna " <<  MySum(a,b) << endl;  
+            break;
+
+         case 2:  
+            cout << "Funkciya v razrabotke " << endl;  
+            break;
+
+		case 3:  
+            cout << "Funkciya v razrabotke " << endl;  
+            break;
+
+		case 4:  
+            cout << "Funkciya v razrabotke " << endl;  
+            break;
+
+         default:  
+            break;  
+      }  
+
+system("pause");
+return 0;
+}
---
+++ funcs.h
@@ -0,0 +1,5 @@
+// заголовочный файл funcs.h
+#ifndef __FUNCS_H__
+#define __FUNCS_H__
+double MySum(double, double);
+#endif // __FUNCS_H__
